816bb810957666b2a5847b006eef6b5f749d8c0c
MatthewTiopes <matthew_tiopes@dlsu.edu.ph>
2025-11-24 21:53:41 +0800

    Add files via upload

---
+++ performance_test.bat
@@ -0,0 +1,4 @@
+nasm -f win64 MP2_ASM_Tiopes.asm
+gcc -c MP2_C_Tiopes-main.c -o MP2_C_Tiopes-main.obj -m64
+gcc MP2_C_Tiopes-main.obj MP2_ASM_Tiopes.obj -o MP2_C_Tiopes-main.exe -m64
+MP2_C_Tiopes-main.exe
